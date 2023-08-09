f816e483425e35fe926688da66d6d8921be78898
sanxzkid <f.sanxz@gmail.com>
2023-08-09 23:52:55 +0000

    add 049_group_anagrams

---
+++ python/049_group_anagrams.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+from typing import List
+
+def group_anagrams(strs: List[str]) -> List[List[str]]:
+    """Function printing python version."""
+    groups = defaultdict(list)
+    for s in strs:
+        key = "".join(sorted(s))
+        groups[key].append(s)
+    return [groups.values()
+
+if __name__ == '__main__':
+    ans = group_anagrams(["asd", "dsa", "das","sda", "dds", "dsd"])
+    print(ans)
